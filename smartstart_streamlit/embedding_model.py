d6aa72720c4a85cc1ae613caef3a74c7ce4925f4
Pedzisai Poseni <pedzisaiposeni@gmail.com>
2025-10-28 14:37:15 +0200

    fixing errors

--- smartstart_streamlit/embedding_model.py
+++ smartstart_streamlit/embedding_model.py
@@ -1,8 +1,8 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 # Build HuggingFace embedding model
 def get_embedding_model():
     return HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2",
-        model_kwargs={'device': 'cpu'}
+        model_kwargs={"device": "cpu"}
     )
--- smartstart_streamlit/llm_setup.py
+++ smartstart_streamlit/llm_setup.py
@@ -1,13 +1,14 @@
-from langchain_community.llms import Together
+from langchain_together import Together
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv("TOGETHER_API_KEY")
 
 def get_llm():
+    api_key = os.getenv("TOGETHER_API_KEY")
     return Together(
         model="mistralai/Mistral-7B-Instruct-v0.1",
         temperature=0.20,
-        max_tokens=150
+        max_tokens=150,
+        together_api_key=api_key
     )
