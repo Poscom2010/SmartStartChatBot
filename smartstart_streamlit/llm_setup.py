993e31f38bbfc374d7a5e2d63808d3b183254eb0
Pedzisai Poseni <pedzisaiposeni@gmail.com>
2025-10-28 14:42:07 +0200

    Resolve deployment failures

--- smartstart_streamlit/llm_setup.py
+++ smartstart_streamlit/llm_setup.py
@@ -1,14 +1,12 @@
-from langchain_together import Together
+from langchain_community.llms import Together
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_llm():
-    api_key = os.getenv("TOGETHER_API_KEY")
     return Together(
         model="mistralai/Mistral-7B-Instruct-v0.1",
         temperature=0.20,
-        max_tokens=150,
-        together_api_key=api_key
+        max_tokens=150
     )
