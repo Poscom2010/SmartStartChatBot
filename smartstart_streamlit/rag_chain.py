09df3b8a8471e46cd3a3fdfcbe2f93716dc30466
Pedzisai Poseni <pedzisaiposeni@gmail.com>
2025-10-28 13:59:10 +0200

    Updatedrag_cahin_py to fix deployment issues

--- smartstart_streamlit/rag_chain.py
+++ smartstart_streamlit/rag_chain.py
@@ -1,5 +1,9 @@
-
-from langchain.chains import RetrievalQA
+try:
+    # Older/compatible import path
+    from langchain.chains import RetrievalQA
+except Exception:
+    # Newer langchain versions expose RetrievalQA in a submodule
+    from langchain.chains.retrieval_qa.base import RetrievalQA
 
 def get_qa_chain(llm, retriever):
     return RetrievalQA.from_chain_type(
